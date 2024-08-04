324b13811111c451b35041ecb1638891827cba25
MichaelPardo31 <mpardo878@soyudemedellin.edu.co>
2024-08-04 16:42:57 -0500

    primer commit

---
+++ programa_1.py
@@ -0,0 +1,4 @@
+#Crear un programa que pida al usuario ingresar su nombre y edad y los imprima en pantalla.
+nombre = input("Ingrese el nombre del usuario: ")
+edad = input("Ingrese el edad: ")
+print(nombre, edad)
